50782fbf7785a846248a324e874a66abdd057b33
Baqar Ali <baqarali789@gmail.com>
2019-12-16 16:09:59 +0500

    Updates

---
+++ precache-manifest.0b2351b4a04729718c178ac8efdca5d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0643acf90af3c6b5c26018c27f7145",
+    "url": "/RobofriendsApp.github.io/index.html"
+  },
+  {
+    "revision": "ca6495cb42938b6edcd7",
+    "url": "/RobofriendsApp.github.io/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a0a511f4ac534299ddde",
+    "url": "/RobofriendsApp.github.io/static/css/main.0d7d2296.chunk.css"
+  },
+  {
+    "revision": "ca6495cb42938b6edcd7",
+    "url": "/RobofriendsApp.github.io/static/js/2.7189d004.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RobofriendsApp.github.io/static/js/2.7189d004.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0a511f4ac534299ddde",
+    "url": "/RobofriendsApp.github.io/static/js/main.a27987cc.chunk.js"
+  },
+  {
+    "revision": "bbb8ba0e4744e0e5f6ed",
+    "url": "/RobofriendsApp.github.io/static/js/runtime-main.cc0e0368.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobofriendsApp.github.io/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './Containers/App'
-import './index.css'
-import 'tachyons'
-
-ReactDom.render(<App /> , document.getElementById("root"));
